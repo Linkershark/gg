0a0ab607b742ef2240b7bc88835ba96899361780
Linkershark <104266098+Linkershark@users.noreply.github.com>
2023-02-25 21:35:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Linkershark//aio/permission/ip | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Linkershark/gg/aio/permission/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
